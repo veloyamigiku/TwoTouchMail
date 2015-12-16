9e42ecd5361800204437e12c9755da2d5758d4d2
xda-yoshioka <xda-yoshioka@kddilabs.jp>
2015-12-16 10:48:20 +0900

    メール作成画面(むかえにきて)を実装します。

--- app/src/main/java/jp/co/myself/twotouchmail/PickUpActivity.java
+++ app/src/main/java/jp/co/myself/twotouchmail/PickUpActivity.java
@@ -1,9 +1,18 @@
 package jp.co.myself.twotouchmail;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class PickUpActivity extends AppCompatActivity {
 
@@ -11,6 +20,27 @@ public class PickUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_up);
+        Button btnSend = (Button) findViewById(R.id.button);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rgPlace = (RadioGroup) findViewById(R.id.rg_place);
+                int checkedId = rgPlace.getCheckedRadioButtonId();
+                String strPlace = ((RadioButton)findViewById(checkedId)).getText().toString();
+                Log.d("place:", strPlace);
+                EditText edit01 = (EditText) findViewById(R.id.editText);
+                String title = edit01.getText().toString();
+
+                Resources res = getResources();
+                Uri uri = Uri.parse("mailto:" + res.getString(R.string.mail_to).toString());
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra(Intent.EXTRA_SUBJECT, title);
+                intent.putExtra(Intent.EXTRA_TEXT, strPlace + "に迎えに来て");
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
